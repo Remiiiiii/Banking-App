d1a2f218959cb0138ca58c63ac1d4c9e97c47677
Remiiiiii <tuf09783@temple.edu>
2023-06-09 19:56:37 -0400

    first commit

---
+++ src/main/java/com/freelance/App.java
@@ -0,0 +1,15 @@
+package com.freelance;
+
+/**
+ * --Full stack path
+ * --1. create your db schema and validate that all operations work
+ * --2. have your models in java, mimic your db tables
+ * --3. create your dao layer
+ * --4. create your views
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
